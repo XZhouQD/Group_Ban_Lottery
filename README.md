21d2be0455a97d478aaec05ba95435daa6888c29
Xiaowei Zhou <xiaowei.zhou@unsw.edu.au>
2020-09-15 17:53:39 +0800

    Bump to 0.2.0 Beta

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Group_Ban_Lottery
+<div align="center">
+<h1>Group_Ban_Lottery</h1>
+
+[![](https://img.shields.io/github/license/XZhouQD/Group_Ban_Lottery?style=for-the-badge)](https://github.com/XZhouQD/Group_Ban_Lottery/blob/master/LICENSE)
+[![Nonebot Version](https://img.shields.io/badge/nonebot-1.7.0+-green.svg?style=for-the-badge)](https://pypi.python.org/pypi/nonebot)
+![Python Version](https://img.shields.io/badge/python-3.7+-blue.svg?style=for-the-badge)
+![CQHTTP Version](https://img.shields.io/badge/cqhttp-4.8+-black.svg?style=for-the-badge)
+
+</div>
+
+## How to use
+1. Add group_ban_lottery.py to your plugins/ directory
+
+1. Add your group id to ENABLE_GROUPS list
+
+1. Set your bot to Group Admin
+
+1. Restart your bot and enjoy!
--- group_ban_lottery.py
+++ group_ban_lottery.py
@@ -9,20 +9,12 @@ Group Ban Lottery Plugin
 A Nonebot Plugin
 
 Version:
-0.1.0-Beta
+0.2.0-Beta
 """
 #Available Groups
 ENABLE_GROUPS=[111111111,22222222]
 
-
-@on_command('lottery', aliases=('抽奖'), permission=permission.GROUP_MEMBER, only_to_me=False)
-async def lottery(session: CommandSession):
-    bot = get_bot()
-    group = session.event.group_id
-    if group is None or group not in ENABLE_GROUPS:
-        return
-    user = session.event['user_id']
-
+def get_length():
     level = random.randint(1, 100)
     if level < 50:
         length = random.randint(1, 1440)
@@ -34,6 +26,17 @@ async def lottery(session: CommandSession):
         length = random.randint(4320, 7200)
     else:
         length = 30*24*60
+    return length
+
+
+@on_command('lottery', aliases=('抽奖'), permission=permission.GROUP_MEMBER, only_to_me=False)
+async def lottery(session: CommandSession):
+    bot = get_bot()
+    group = session.event.group_id
+    if group is None or group not in ENABLE_GROUPS:
+        return
+    user = session.event['user_id']
+    length = get_length()
     try:
         log.logger.info(f"[GB_Lottery]{group}, {user}, {length}")
         await bot.set_group_ban(group_id=group, user_id=user, duration=length * 60)
@@ -49,3 +52,35 @@ async def lottery(session: CommandSession):
 @lottery.args_parser
 async def _(session: CommandSession):
     pass
+
+
+@on_command('give_lottery', aliases=('帮你抽奖'), permission=permission.GROUP_ADMIN, only_to_me=False)
+async def give_lottery(session: CommandSession):
+    bot = get_bot()
+    group = session.event.group_id
+    if group is None or group not in ENABLE_GROUPS:
+        return
+
+    user = session.get('target')
+    length = get_length()
+    try:
+        log.logger.info(f"[GB_Lottery]{group}, {user}, {length}")
+        await bot.set_group_ban(group_id=group, user_id=user, duration=length * 60)
+    except CQHTTPError:
+        pass
+    try:
+        message = f"[CQ:at,qq={user}]抽奖成功！！你抽中的是{length}分钟禁言套餐！"
+        await session.send(message)
+    except CQHTTPError:
+        pass
+
+
+@give_lottery.args_parser
+async def give_lottery_parser(session: CommandSession):
+    stripped_arg = session.current_arg.strip()
+    at = session.get
+
+    if session.is_first_run:
+        if stripped_arg:
+            session.state['target'] = int(stripped_arg.split('=')[1][:-1])
+        return
